538fb948475aa2ab4b3bbe9c2aab853661c23762
Chukwuka Rosemary Uche <chukwukarosemary2020@gmail.com>
2026-03-25 20:52:33 +0000

    Bug 1948019 - Add Forget About This Site option to Firefox View history context menu r=nsharpley,fxview-reviewers,fluent-reviewers,bolsson
    Differential Revision: https://phabricator.services.mozilla.com/D289065

--- browser/components/firefoxview/history.mjs
+++ browser/components/firefoxview/history.mjs
@@ -152,6 +152,20 @@ class HistoryInView extends ViewPage {
     }
   }
 
+  async forgetAboutThisSite() {
+    let host = Services.io.newURI(this.triggerNode.url).host;
+    let baseDomain;
+    try {
+      baseDomain = Services.eTLD.getBaseDomainFromHost(host);
+    } catch (e) {
+      // If there is no baseDomain we fall back to host
+    }
+    await this.getWindow().gDialogBox.open(
+      "chrome://browser/content/places/clearDataForSite.xhtml",
+      { host, hostOrBaseDomain: baseDomain ?? host }
+    );
+  }
+
   onSecondaryAction(e) {
     this.triggerNode = e.originalTarget;
     this.panelList.toggle(e.detail.originalEvent);
@@ -244,6 +258,11 @@ class HistoryInView extends ViewPage {
         <panel-item
           @click=${this.deleteFromHistory}
           data-l10n-id="firefoxview-history-context-delete"
+          data-l10n-attrs="accesskey"
+        ></panel-item>
+        <panel-item
+          @click=${this.forgetAboutThisSite}
+          data-l10n-id="firefoxview-history-context-forget-site"
           data-l10n-attrs="accesskey"
         ></panel-item>
         <hr />
