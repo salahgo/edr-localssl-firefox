ab01648ca5877ef5271702d8e9f894971e735af3
Anshul <malikanshul29@gmail.com>
2026-03-30 16:39:02 +0000

    Bug 2027740 - Relax ice-ufrag/ice-pwd character validation to only reject '$'. r=bwc
    Differential Revision: https://phabricator.services.mozilla.com/D290924

--- dom/media/webrtc/sdp/SdpHelper.cpp
+++ dom/media/webrtc/sdp/SdpHelper.cpp
@@ -741,9 +741,10 @@ static constexpr size_t kMaxIceUfragLength = 256;
 static constexpr size_t kMinIcePwdLength = 22;
 static constexpr size_t kMaxIcePwdLength = 256;
 
+// Bug 2027782: Google Meet uses '=' in ice-pwd, only reject '$' for now.
 static bool IsValidIceToken(const std::string& aToken) {
   for (unsigned char c : aToken) {
-    if (!isalnum(c) && c != '+' && c != '/') {
+    if (c == '$') {
       return false;
     }
   }
